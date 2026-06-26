4ca0219eebe9f108694e436911bbb4b5e6e97e0c
SUYANS22 <suyans8825@student.ku.edu.np>
2026-06-26 15:53:20 +0545

    Update task.cpp

--- task.cpp
+++ task.cpp
@@ -198,8 +198,9 @@ void checkReminders() {
             deadline_tm.tm_mon = t.dueMonth - 1;
             deadline_tm.tm_mday = t.dueDay;
             time_t deadline_time = mktime(&deadline_tm);
-            double secondsLeft = difftime(deadline_time, now);
-            double daysLeft = secondsLeft / (24 * 3600);
+            int secondsLeft = difftime(deadline_time, now);
+            int daysLeft =static_cast<int>( secondsLeft / (24 * 3600));
+            int hoursleft=static_cast<int>((secondsLeft- daysLeft*24*3600)/3600);
             if (daysLeft <= 2.0) {
                 if (!urgencyFound) {
                     cout << "\n========================================\n";
